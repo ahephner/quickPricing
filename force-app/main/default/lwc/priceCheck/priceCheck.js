531de5d0372d3cb1482e7413ace5cd88e4aad458
ahephner <ajhephner178@gmail.com>
2023-04-10 14:46:40 -0400

    able to search and display data

--- force-app/main/default/lwc/priceCheck/priceCheck.js
+++ force-app/main/default/lwc/priceCheck/priceCheck.js
@@ -1,5 +1,73 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,track,api } from 'lwc';
+import FORM_FACTOR from '@salesforce/client/formFactor';
+import checkPrice from '@salesforce/apex/quickPriceSearch.getPricing';
+//import inCounts from '@salesforce/apex/cpqApex.inCounts';
+import {newInventory,allInventory} from 'c/helper' 
 
 export default class PriceCheck extends LightningElement {
-    title = 'hello'
+    searchTerm;
+    priceBook = '01s410000077vSKAAY';
+    loaded; 
+    formSize;
+    isPinned = false; 
+    @track pinnedCards = [];
+    @track prod = [];
+    
+    connectedCallback(){ 
+        this.formSize = this.screenSize(FORM_FACTOR);
+        this.loaded = true;     
+    }
+
+    screenSize = (screen) => {
+        return screen === 'Large'? true : false  
+    }
+
+    handleKeys(evt){
+        let enterKey = evt.keyCode === 13;
+        if(enterKey){
+            this.searchTerm = evt.target.value;
+            this.handleSearch();
+        }
+    }
+
+    handleSearch(){
+        this.searchTerm = this.template.querySelector('[data-value="searchInput"]').value
+        if(this.searchTerm.length<3){
+            //add lwc alert here
+            return;
+        }
+        this.loaded = false
+        checkPrice({priceBookId: this.priceBook, searchKey: this.searchTerm})
+        .then((res)=>{
+            let name;
+                let cost; 
+                let flr;
+                let lev1;
+                let lev2;
+                let stock;
+                let allStock;
+                let ProductCode;  
+                let url; 
+                this.prod = res.map(x=>{
+                    name= x.Product2.Name + ' - '+ x.Product2.ProductCode,
+                    cost = x.Agency_Product__c ? 'Agency' : x.Product_Cost__c,
+                    flr = x.Floor_Price__c,
+                    lev1 = x.Level_1_UserView__c,
+                    lev2 = x.Level_2_UserView__c,
+                    stock = x.Product2.Product_Status__c,
+                    allStock = x.Product2.Total_Product_Items__c
+                    ProductCode = x.Product2.ProductCode,
+                    url = 'https://advancedturf.lightning.force.com/lightning/r/'+x.Product2Id+'/related/ProductItems/view'
+                    return {...x, name, cost, flr, lev1, lev2, stock, allStock, ProductCode,url}
+                })
+
+        }).then(()=>{
+            this.loaded = true; 
+            this.searchTerm = ''; 
+            let x = this.template.querySelector('lightning-input').value;
+            x = ''; 
+            console.log(JSON.stringify(this.prod));
+            
+        })
+    }
 }
