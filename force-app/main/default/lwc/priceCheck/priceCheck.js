f81ef0b7ae67754a49f3624dc3be3bb4a8f1fb75
ahephner <ajhephner178@gmail.com>
2023-04-24 15:12:33 -0400

    sent to full for inventory/mobile testing. Added ability to search for inventory

--- force-app/main/default/lwc/priceCheck/priceCheck.js
+++ force-app/main/default/lwc/priceCheck/priceCheck.js
@@ -1,7 +1,7 @@
 import { LightningElement,track,api } from 'lwc';
 import FORM_FACTOR from '@salesforce/client/formFactor';
 import checkPrice from '@salesforce/apex/quickPriceSearch.getPricing';
-//import inCounts from '@salesforce/apex/cpqApex.inCounts';
+import inCounts from '@salesforce/apex/cpqApex.inCounts';
 import {newInventory,allInventory, roundNum} from 'c/helper' 
 
 export default class PriceCheck extends LightningElement {
@@ -11,8 +11,9 @@ export default class PriceCheck extends LightningElement {
     formSize;
     isPinned = false; 
     showWarn = false; 
-    demoMode = false; 
-    demoLabel = 'Normal'
+    btnLabel = 'Inventory';
+    showInventory = false; 
+    warehouse; 
     @track pinnedCards = [];
     @track prod = [];
     @track tagCards = []; 
@@ -134,8 +135,84 @@ export default class PriceCheck extends LightningElement {
         
     }
 
+//Invetory Section
     checkInv(){
-        console.log('check inv');
+        if(this.prod.length<1){
+            alert('must have found at least 1 product');
+            return; 
+        }
+        if(!this.showInventory){ 
+            this.showInventory = true; 
+            this.btnLabel = 'Back to Products';
+        }else{ 
+            this.showInventory = false;
+            this.btnLabel = 'Check Inventory';
+        }
+        
+    }
+
+    get warehouseOptions(){
+        return [
+            {label:'All', value:'All'},
+            {label: '105 | Noblesville', value:'1312M000000PB0ZQAW'}, 
+            {label:'115 | ATS Ingalls', value:'1312M00000001nsQAA'},
+            {label:'125 | ATS Lebanon (Parts)', value:'1312M00000001ntQAA'},
+            {label:'200 | ATS Louisville', value:'1312M00000001nuQAA'},
+            {label:'250 | ATS Florence', value:'1312M00000001nvQAA'},
+            {label:'270 | ATS Winston-Salem', value:'1312M00000001nwQAA'},
+            {label:'310 | ATS Tomball', value:'1312M000000PB6AQAW'},
+            {label:'360 | ATS Nashville', value:'1312M00000001nxQAA'},
+            {label:'400 | ATS Columbus', value:'1312M00000001nyQAA'},
+            {label:'415 | ATS Sharonville', value:'1312M00000001nzQAA'},
+            {label:'440 | ATS Lewis Center', value:'1312M00000001o0QAA'},
+            {label:'450 | ATS Brecksville', value:'1312M00000001o1QAA'},
+            {label:'470 | ATS Boardman', value:'1312M00000001o2QAA'},
+            {label:'510 | ATS Travis City', value:'1312M00000001o3QAA'},
+            {label:'520 | ATS Farmington Hills', value:'1312M00000001o4QAA'},
+            {label:'600 | ATS - Elkhart', value:'1312M00000001o5QAA'},
+            {label:'710 | ATS - St. Peters', value:'1312M00000001o6QAA'},
+            {label:'720 | ATS - Cape Girardeau', value:'1312M00000001o7QAA'},
+            {label:'730 | ATS - Columbia', value:'1312M00000001o8QAA'},
+            {label:'770 | ATS - Riverside', value:'1312M00000001o9QAA'},
+            {label:'790 | ATS - Springfield', value:'1312M0000004D7IQAU'},
+            {label:'820 | ATS - Wheeling', value:'1312M000000PB0UQAW'},
+            {label:'850 | ATS - Madison', value:'1312M00000001oAQAQ'},
+            {label:'860 | ATS - East Peoria', value:'1312M000000PB2BQAW'},
+            {label:'960 | ATS - Monroeville', value:'1312M00000001oBQAQ'},
+            {label:'980 | ATS - Ashland', value:'1312M00000001oCQAQ'},
+            {label:'999 | ATS - Fishers', value:'1312M000000PB3FQAW'}
+        ];
+    }
+
+    async checkInventory(locId){
+        this.warehouse = locId.detail.value; 
+        this.loaded = false;
+        let data = [...this.prod];
+        let pcSet = new Set();
+        let prodCodes = [];
+        try{
+            data.forEach(x=>{
+                pcSet.add(x.ProductCode);
+            })
+            prodCodes = [...pcSet];
+
+            let inCheck = await inCounts({pc:prodCodes, locId:this.warehouse});
+           console.log('inCheck ' +JSON.stringify(inCheck));
+            this.prod = this.warehouse === 'All' ? await allInventory(data, inCheck) : await newInventory(data, inCheck);
+            //this will cause rerender to run so we can update the warning colors. 
+            //console.log('back')
+            console.log(JSON.stringify(this.prod)); 
+        }catch(error){
+            this.error = error;
+            const evt = new ShowToastEvent({
+                title: 'Error loading inventory',
+                message: this.error,
+                variant: 'warning'
+            });
+            this.dispatchEvent(evt);
+        }finally{
+            this.loaded = true;
+        }    
         
     }
 }
