652a94648d1d408210f731961ee9fc6aeec6897a
ahephner <ajhephner178@gmail.com>
2023-10-25 14:37:41 -0400

    adding lwc helper apex for common functions. Started using them for inventory search

--- force-app/main/default/lwc/inventoryCheck/inventoryCheck.js
+++ force-app/main/default/lwc/inventoryCheck/inventoryCheck.js
@@ -4,6 +4,7 @@ import getWarehouses from '@salesforce/apex/lwcHelper.getWarehouse';
 import userWareHouse from '@salesforce/apex/lwcHelper.userLocation';
 import warehouseInventory from '@salesforce/apex/quickPriceSearch.warehouseInventory';
 import getPickListValues from '@salesforce/apex/lwcHelper.getPickListValues'
+import multiplePicklists from '@salesforce/apex/lwcHelper.multiplePicklists2'
 import userId from '@salesforce/user/Id';
 import FORM_FACTOR from '@salesforce/client/formFactor';
 import LightningAlert from 'lightning/alert';
@@ -38,14 +39,22 @@ export default class InventoryCheck extends LightningElement {
     }
 
     async init(){
-        let funStart = performance.now(); 
         let back = await getWarehouses()
         this.userwareHouseNumb = await userWareHouse({userId: this.userId});
-        let pickListStart = performance.now();
-        this.primaryValues = await getPickListValues({objName: 'Product2', fieldAPI: 'Primary_Category__c'});
-        this.subValues = await getPickListValues({objName: 'Product2', fieldAPI:'Subcategory__c'})
+        //let funStart = performance.now(); 
+        //multiplePicklists pass in single object in objectName then all picklist fields you want back
+        //whatever fieldname you pass that will come back as a key with the picklist labels and values as value 
+        //there also is a single picklist method in same class getPickListValues works same way except you pass one field 
+        let picVals = await multiplePicklists({objectName:'Product2', picklistFields:'Primary_Category__c,Subcategory__c'})
+        this.primaryValues = picVals.Primary_Category__c;
+        this.subValues = picVals.Subcategory__c; 
+        //let funStop = performance.now();
+        //console.log(1,test)
+        //let pickListStart = performance.now();
+        //this.primaryValues = await getPickListValues({objName: 'Product2', fieldAPI: 'Primary_Category__c'});
+        //this.subValues = await getPickListValues({objName: 'Product2', fieldAPI:'Subcategory__c'})
         this.primCat = this.primaryValues[0].value;  
-        let pickListEnd = performance.now();
+        //let pickListEnd = performance.now();
         let mapped = await back.map((item, index) =>({
                                     ...item, 
                                     label:item.Name, 
@@ -57,9 +66,8 @@ export default class InventoryCheck extends LightningElement {
         let start = this.warehouseOptions.filter((x)=> x.label.includes(this.userwareHouseNumb))
         this.selWareHouse = start[0].value ? start[0].value : this.warehouseOptions[0].value; 
         this.loaded = true; 
-        let funStop = performance.now();
-        console.log(`Picklist Time: ${pickListEnd- pickListStart}`);
-        console.log(`Total Function: ${funStop - funStart}`);
+        //console.log(`test time: ${funStop - funStart}`);
+        //console.log(`Picklist Time: ${pickListEnd- pickListStart}`);
         
     }
 
@@ -90,7 +98,8 @@ export default class InventoryCheck extends LightningElement {
         this.prodData = query.map((item)=>({
             ...item,
             name: item.Product2.Name + ' ' + item.Product_Code__c,
-            inv: `On Hand: ${item.QuantityOnHand}   Allocated: ${item.Quantity_Allocated__c}   Available: ${item.Quantity_Available__c}`,
+            allocated: item.Quantity_Allocated__c === undefined? 0 : item.Quantity_Allocated__c,
+           // inv: `On Hand: ${item.QuantityOnHand}   Allocated: ${allocated}   Available: ${item.Quantity_Available__c}`,
             url: 'https://advancedturf--full.sandbox.lightning.force.com/lightning/r/Product2/'+item.Product2Id+'/related/ProductItems/view'
         }))
         this.loaded = true; 
